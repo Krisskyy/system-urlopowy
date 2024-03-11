34054cdf56e5a588118310d6fcbee11c811091a0
Krisskyy <kriss060428@gmail.com>
2024-03-11 14:59:20 +0100

    Created login page

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="photo column">
+    <img src="../assets/holiday-logo.svg" alt="Logo" />
+    <img src="../assets/login-picture.svg" alt="Obrazek logowania" />
+    <h1>Witamy!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at
+      dapibus sapien.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.photo {
+  width: 60%;
+  background-color: #f1f1f1;
+  padding: 50px 48px;
+  border-radius: 10px 0px 0px 10px;
+}
+img:first-of-type {
+  width: 113px;
+  margin-bottom: 40px;
+}
+
+h1 {
+  font: normal normal 600 28px/24px Poppins;
+  padding: 48px 0 12px 0;
+}
+p {
+  font: normal normal 16px/24px Poppins;
+}
+</style>
---
+++ src/layouts/LoginPageLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout class="container flex flex-center">
+    <div class="box flex">
+      <LoginComponent />
+      <div class="login">
+        <q-page-container>
+          <router-view />
+        </q-page-container>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script setup>
+import LoginComponent from "src/components/LoginComponent.vue";
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+.container {
+  background: #f6f8fc;
+}
+
+.box {
+  width: 1200px;
+}
+
+.login {
+  background: #ffffff;
+  border-radius: 0px 10px 10px 0px;
+  width: 40%;
+  padding: 106px 41px 124px 50px;
+}
+</style>
