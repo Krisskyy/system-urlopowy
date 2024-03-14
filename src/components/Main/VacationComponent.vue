2e1258b178bdd54704c5071581c7c5a6124abf39
Krisskyy <kriss060428@gmail.com>
2024-03-14 14:07:32 +0100

    Main Pages

---
+++ src/components/Main/VacationComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="box">
+    <div class="icons flex justify-end">
+      <img src="src/assets/edit-ico.svg" alt="edit icon" />
+    </div>
+    <div class="box-header flex">
+      <div class="circle"></div>
+      <div class="rest flex items-center justify-between">
+        <h3>Magda Kowalska <br /><span>UX/UI designer</span></h3>
+        <p>Urlop wypoczynkowy</p>
+      </div>
+    </div>
+    <div class="box-footer flex justify-around">
+      <div class="left flex justify-left">
+        <h4>Od <span>22.03.2022r.</span></h4>
+      </div>
+      <div class="right flex flex-center">
+        <h4>Do <span>22.03.2022r.</span></h4>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.box {
+  width: 100%;
+  height: 100%;
+  background-color: $classic;
+  .icons {
+    img {
+      margin: 10px 10px 0 0;
+    }
+  }
+  .box-header {
+    padding: 11px 5px 20px 47px;
+    .circle {
+      background-color: #ffcb91;
+      width: 20px;
+      height: 20px;
+      border-radius: 100%;
+    }
+    .rest {
+      width: calc(100% - 25px);
+      padding: 0 30px 0 10px;
+      margin-top: 0;
+      h3 {
+        font: normal normal 600 14px Poppins;
+        margin-top: 0;
+        margin-bottom: 0;
+        span {
+          font: normal normal 500 12px Poppins;
+        }
+      }
+      p {
+        font: normal normal 600 12px Poppins;
+        background-color: #d8fff9;
+        margin-bottom: 0;
+        padding: 0 5px;
+      }
+    }
+  }
+  .box-footer {
+    padding: 0 35px 0 77px;
+    border-top: 1px solid #e0e3e5;
+    h4 {
+      font: normal normal 500 10px Poppins;
+      color: #6c6666;
+      span {
+        font: normal normal 600 12px Poppins;
+        color: #000000;
+      }
+    }
+    .left {
+      border-right: 1px solid #e0e3e5;
+      width: 50%;
+      padding-bottom: 11px;
+    }
+    .right {
+      width: 50%;
+      padding-bottom: 11px;
+    }
+  }
+}
+</style>
--- src/pages/Main/RequestsPage.vue
+++ src/pages/Main/RequestsPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="content">
-    <div class="content-header flex justify-between items-center">
+  <div class="content-request">
+    <div class="request-header flex justify-between items-center">
       <h1>Wnioski</h1>
       <a href="#" class="flex items-center">
         Historia wniosków
@@ -15,10 +15,146 @@
       />
     </div>
   </div>
+  <div class="content-vacation">
+    <h1>Nadchodzące urlopy pracowników</h1>
+    <div class="vacation-header">
+      <table></table>
+      <div class="buttons">
+        <tr>
+          <th>TYP WNIOSKU</th>
+          <th>PRACOWNIK</th>
+          <th>STANOWISKO</th>
+        </tr>
+        <tr>
+          <td>
+            <q-select
+              dense
+              outlined
+              bg-color="white"
+              class="vacation-select"
+              v-model="modelFirst"
+              :options="options"
+              multiple
+              emit-value
+              map-options
+            >
+              <template
+                v-slot:option="{ itemProps, opt, selected, toggleOption }"
+              >
+                <q-item v-bind="itemProps">
+                  <q-item-section>
+                    <q-checkbox
+                      :label="opt.label"
+                      :model-value="selected"
+                      @update:model-value="toggleOption(opt)"
+                    />
+                  </q-item-section>
+                </q-item>
+              </template>
+              <template v-slot:selected>
+                {{ computedSelectedLabelFirst }}
+              </template>
+            </q-select>
+          </td>
+          <td>
+            <q-select
+              dense
+              outlined
+              bg-color="white"
+              class="vacation-select"
+              v-model="modelSecond"
+              :options="optionsSecond"
+              multiple
+              emit-value
+              map-options
+            >
+              <template
+                v-slot:option="{ itemProps, opt, selected, toggleOption }"
+              >
+                <div style="width: 300px">
+                  <q-input
+                    v-model="text"
+                    label="Wyszukaj"
+                    style="padding: 3px"
+                    v-show="opt.input"
+                  >
+                    <template v-slot:append>
+                      <q-icon name="search" style="margin-right: 5px" />
+                    </template>
+                  </q-input>
+                  <q-item v-bind="itemProps">
+                    <q-item-section>
+                      <q-checkbox
+                        :label="opt.label"
+                        :model-value="selected"
+                        @update:model-value="toggleOption(opt)"
+                      />
+                    </q-item-section>
+                  </q-item>
+                </div>
+              </template>
+              <template v-slot:selected>
+                {{ computedSelectedLabelSecond }}
+              </template>
+            </q-select>
+          </td>
+          <td>
+            <q-select
+              dense
+              outlined
+              bg-color="white"
+              class="vacation-select"
+              v-model="modelThird"
+              :options="options"
+              multiple
+              emit-value
+              map-options
+            >
+              <template
+                v-slot:option="{ itemProps, opt, selected, toggleOption }"
+              >
+                <q-item v-bind="itemProps">
+                  <q-item-section>
+                    <q-checkbox
+                      :label="opt.label"
+                      :model-value="selected"
+                      @update:model-value="toggleOption(opt)"
+                    />
+                  </q-item-section>
+                </q-item>
+              </template>
+              <template v-slot:selected>
+                {{ computedSelectedLabelThird }}
+              </template>
+            </q-select>
+          </td>
+        </tr>
+        <div class="filter-buttons flex justify-end">
+          <q-btn class="buttonFirst" flat label="Wyczyść filtry" />
+          <q-btn
+            outline
+            style="color: #703ada"
+            label="Filtruj"
+            class="buttonSecond"
+          />
+        </div>
+        <div class="vacation-component">
+          <VacationComponent />
+          <VacationComponent />
+          <VacationComponent />
+          <VacationComponent />
+          <VacationComponent />
+          <VacationComponent />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import RequestBoxComponent from "src/components/Main/RequestBoxComponent.vue";
+import VacationComponent from "src/components/Main/VacationComponent.vue";
+import { ref, computed } from "vue";
 
 const boxes = [
   {
@@ -34,21 +170,117 @@ const boxes = [
     color: "#FF8C8C",
   },
 ];
+
+const modelFirst = ref([]);
+const modelSecond = ref([]);
+const modelThird = ref([]);
+const text = ref([]);
+
+const options = [
+  {
+    label: "Urlop wypoczynkowy",
+  },
+  {
+    label: "Urlop na żądanie",
+  },
+  {
+    label: "L4",
+  },
+  {
+    label: "Urlop wychowawczy",
+  },
+  {
+    label: "Urlop macierzyński",
+  },
+  {
+    label: "Urlop okolicznościowy",
+  },
+];
+
+const optionsSecond = [
+  {
+    label: "Zaznacz wszystkich",
+    input: true,
+    click: "toggleAllCheckboxes",
+  },
+  {
+    label: "Karolina Nowak1",
+    input: false,
+    click: "",
+  },
+  {
+    label: "Jan Kowalski1",
+    input: false,
+    click: "",
+  },
+  {
+    label: "Karolina Nowak2",
+    input: false,
+    click: "",
+  },
+  {
+    label: "Jan Kowalski2",
+    input: false,
+    click: "",
+  },
+  {
+    label: "Karolina Nowak3",
+    input: false,
+    click: "",
+  },
+  {
+    label: "Jan Kowalski3",
+    input: false,
+    click: "",
+  },
+];
+
+const optionsThird = [
+  {
+    label: "Karolina Nowak",
+  },
+  {
+    label: "Jan Kowalski",
+  },
+  {
+    label: "Karolina Nowak",
+  },
+  {
+    label: "Jan Kowalski",
+  },
+  {
+    label: "Karolina Nowak",
+  },
+  {
+    label: "Jan Kowalski",
+  },
+];
+
+const computedSelectedLabelFirst = computed(() => {
+  return `Wybrano: ${modelFirst.value.length}`;
+});
+const computedSelectedLabelSecond = computed(() => {
+  return `Wybrano: ${modelSecond.value.length}`;
+});
+const computedSelectedLabelThird = computed(() => {
+  return `Wybrano: ${modelThird.value.length}`;
+});
 </script>
 
 <style lang="scss" scoped>
-.content {
+.content-request {
   height: 85vh;
   background-color: $classic;
   margin: 30px 40px;
   border-radius: 10px;
   padding: 30px 40px;
 
-  .content-header {
+  .request-header {
     margin-bottom: 30px;
 
     h1 {
       font: normal normal 500 28px/36px Poppins;
+      color: #111111de;
     }
 
     a {
@@ -60,6 +292,57 @@ const boxes = [
         margin-left: 20px;
       }
     }
+  }
+}
+
+.content-vacation {
+  background-color: $classic;
+  margin: 30px 40px 0 40px;
+  border-radius: 10px;
+  padding: 30px 40px;
+
+  h1 {
+    font: normal normal 500 28px/42px Poppins;
+    color: #111111de;
+  }
+
+  th {
+    font: normal normal 500 14px/21px Poppins;
+    text-transform: uppercase;
+    text-align: left;
+  }
+
+  th,
+  td {
+    padding: 5px;
+  }
+
+  .vacation-select {
+    font: normal normal 500 14px/21px Poppins;
+    width: 175px;
+    height: 36px;
+  }
+
+  .buttonFirst {
+    font: normal normal 600 14px/16px Poppins;
+    color: #703ada;
+    margin-right: 20px;
+    text-decoration: none;
+  }
+  .buttonSecond {
+    font: normal normal 600 14px/16px Poppins;
+    padding: 10px 24px;
+  }
+
+  .vacation-component {
+    width: 100%;
+    background: $background 0% 0% no-repeat padding-box;
+    border-radius: 4px;
+    margin-top: 38px;
+    padding: 25px 20px;
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-gap: 10px;
   }
 }
 </style>
